6b6a952236e7639eed0d22544406ee21217c9444
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-15 03:46:01 +0100

    Add files via upload
    Building as a separate resource until a time it's integrated into the main resource

---
+++ admintools/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Mascotte'
+description 'Admin NUI for Linus framework'
+version '1.0.0'
+
+client_script "nui.lua"
+
+ui_page "html/index.html"
+
+files {
+    'html/index.html',
+    'html/index.js',
+    'html/index.css',
+    'html/reset.css',
+    'html/assets/img/*.png'
+}
---
+++ admintools/html/index.js
@@ -0,0 +1,53 @@
+$(function () {
+    function display(bool) {
+        if (bool) {
+            $("#site-container").show();
+        } else {
+            $("#site-container").hide();
+        }
+    }
+
+    display(false)
+
+    window.addEventListener('message', function(event) {
+        var item = event.data;
+        if (item.type === "ui") {
+            if (item.status == true) {
+                display(true)
+            } else {
+                display(false)
+            }
+        }
+    })
+    // if the person uses the escape key, it will exit the resource
+    document.onkeyup = function (data) {
+        if (data.which == 27) {
+            $.post('http://admintools/exit', JSON.stringify({}));
+            return
+        }
+    };
+    $("#database-card").click(function () {
+        $.post('http://admintools/exit', JSON.stringify({}));
+        return
+    })
+    //when the user clicks on the submit button, it will run
+    $("#submit").click(function () {
+        let inputValue = $("#input").val()
+        if (inputValue.length >= 100) {
+            $.post("http://admintools/error", JSON.stringify({
+                error: "Input was greater than 100"
+            }))
+            return
+        } else if (!inputValue) {
+            $.post("http://admintools/error", JSON.stringify({
+                error: "There was no value in the input field"
+            }))
+            return
+        }
+        // if there are no errors from above, we can send the data back to the original callback and hanndle it from there
+        $.post('http://admintools/main', JSON.stringify({
+            text: inputValue,
+        }));
+        return;
+    })
+})
