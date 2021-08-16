0e93af070ece885343f85f6341870142ca1bd519
NotSomething0 <programming.unconsociated@aleeas.com>
2021-08-15 21:49:33 -0500

    fix(admintools) continuously check if we should disable game controls

--- admintools/nui.lua
+++ admintools/nui.lua
@@ -1,16 +1,31 @@
-local display = false
-
-RegisterCommand("admin", function(source, args)
-    SetDisplay(not display)
+local isOpen = false
+CreateThread(function()
+  while true do
+    if isOpen then
+      DisableControlAction(0, 1, display) -- LookLeftRight
+      DisableControlAction(0, 2, display) -- LookUpDown
+      DisableControlAction(0, 142, display) -- MeleeAttackAlternate
+      DisableControlAction(0, 18, display) -- Enter
+      DisableControlAction(0, 322, display) -- ESC
+      DisableControlAction(0, 106, display) -- VehicleMouseControlOverride
+    else
+      Wait(500)
+    end
+    Wait(0)
+  end
 end)
 
---very important cb 
+RegisterCommand("admin", function(source, args)
+    SetDisplay(not isOpen)
+end)
+
+--very important cb
 RegisterNUICallback("exit", function(data)
     chat("exited", {0,255,0})
     SetDisplay(false)
 end)
 
--- this cb is used as the main route to transfer data back 
+-- this cb is used as the main route to transfer data back
 -- and also where we hanld the data sent from js
 RegisterNUICallback("main", function(data)
     chat(data.text, {0,255,0})
@@ -23,33 +38,13 @@ RegisterNUICallback("error", function(data)
 end)
 
 function SetDisplay(bool)
-    display = bool
+    isOpen = bool
     SetNuiFocus(bool, bool)
     SendNUIMessage({
         type = "ui",
         status = bool,
     })
 end
-
-
-
-Citizen.CreateThread(function()
-    while display do
-        Citizen.Wait(0)
-        -- https://runtime.fivem.net/doc/natives/#_0xFE99B66D079CF6BC
-        --[[ 
-            inputGroup -- integer , 
-	        control --integer , 
-            disable -- boolean 
-        ]]
-        DisableControlAction(0, 1, display) -- LookLeftRight
-        DisableControlAction(0, 2, display) -- LookUpDown
-        DisableControlAction(0, 142, display) -- MeleeAttackAlternate
-        DisableControlAction(0, 18, display) -- Enter
-        DisableControlAction(0, 322, display) -- ESC
-        DisableControlAction(0, 106, display) -- VehicleMouseControlOverride
-    end
-end)
 
 function chat(str, color)
     TriggerEvent(
