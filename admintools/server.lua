948f5a363bfa6cfed779991681bbf77aff33b302
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-18 14:12:05 +0100

    Admin module updates

--- admintools/server.lua
+++ admintools/server.lua
@@ -1,1 +1,13 @@
+RegisterNetEvent('Admin:RequestPlayerPunishment')
+AddEventHandler('Admin:RequestPlayerPunishment', function(type, playerId, reason)
+    local playerId = tonumber(playerId)
+     if IsPlayerAceAllowed(source, 'ace.perm') then
+    if (type == 'kick') then
+        DropPlayer(playerId, reason)
+    else
+        exports.Valkyrie:banPlayer(playerId, reason)
+    end
+      end
+end)
+
 
