069000d9d3ebf54094b2d8eb9df2d65b8b7d2987
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-18 14:17:39 +0100

    Update cl_events.lua

--- linus/client/cl_events.lua
+++ linus/client/cl_events.lua
@@ -1,1 +1,22 @@
+-- To Trigger Use: TriggerServerEvent('Admin:RequestPlayerList')
+RegisterNetEvent('Admin:RecievePlayerList')
+AddEventHandler('Admin:RecievePlayerList', function(playerList)
+  local players = playerList
+end)
 
+AddEventHandler('playerSpawned', function()
+  if veryfirstspawn then
+  CreateNewPlayerAppearance(source)
+    veryfirstspawn = false
+  else
+    TriggerServerEvent('getcharacterclothes', source)
+  end
+end
+end)
+
+RegisterNetEvent('player:loadcharacterclothes')
+AddEventHandler('player:loadcharacterclothes', function(source, charappearance)
+  local appearance = charappearance
+  exports["fivem-appearance"]:setPlayerAppearance(source, appearance)
+  print('Loaded Outfit')
+end)
