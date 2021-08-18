ddc087715b102399ba8e93e54ac72d5c1cc0528b
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-18 14:18:15 +0100

    Update cl_main.lua

--- linus/client/cl_main.lua
+++ linus/client/cl_main.lua
@@ -1,6 +1,3 @@
-veryfirstspawn = false
-local players = {}
-
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
@@ -11,26 +8,4 @@ Citizen.CreateThread(function()
 			break
 		end
 	end
-end)
--- To Trigger Use: TriggerServerEvent('Admin:RequestPlayerList')
-RegisterNetEvent('Admin:RecievePlayerList')
-AddEventHandler('Admin:RecievePlayerList', function(playerList)
-  local players = playerList
-end)
-
-AddEventHandler('playerSpawned', function()
-  if veryfirstspawn then
-  CreateNewPlayerAppearance(source)
-    veryfirstspawn = false
-  else
-    TriggerServerEvent('getcharacterclothes', source)
-  end
-end
-end)
-
-RegisterNetEvent('player:loadcharacterclothes')
-AddEventHandler('player:loadcharacterclothes', function(source, charappearance)
-  local appearance = charappearance
-  exports["fivem-appearance"]:setPlayerAppearance(source, appearance)
-  print('Loaded Outfit')
 end)
