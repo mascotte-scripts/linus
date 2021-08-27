2e8ab27a097d24ba9805c4761f54e8d73838dd40
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-27 06:29:25 +0100

    Update sv_events.lua

--- linus/server/sv_events.lua
+++ linus/server/sv_events.lua
@@ -1,4 +1,5 @@
 charid = 'char'
+firstspawn = false
 
 RegisterNetEvent('Multichar:InitiateServerSession')
 AddEventHandler('Multichar:InitiateServerSession', function()
@@ -15,23 +16,24 @@ RegisterNetEvent('Multichar:SetupCharacterData')
 AddEventHandler('Multichar:SetupCharacterData', function(CharacterData)
      charid = CharacterData[6]
     local identifier = GetIdentifier(source, charid)
-    print('Saving character data via KVS')
+    firstspawn = true
+        SetResourceKvp(('users:%s:'):format(identifier), identifier)
 		SetResourceKvp(('users:%s:CharacterData:firstname'):format(identifier), CharacterData[1])
 		SetResourceKvp(('users:%s:CharacterData:lastname'):format(identifier), CharacterData[2])
 		SetResourceKvp(('users:%s:CharacterData:gender'):format(identifier), CharacterData[3])
 		SetResourceKvp(('users:%s:CharacterData:nation'):format(identifier), CharacterData[4])
 		SetResourceKvp(('users:%s:CharacterData:dob'):format(identifier), CharacterData[5])
-    print('char saved')
 end)
 
 RegisterNetEvent('Player:GetCharactersOutfit')
 AddEventHandler('Player:GetCharactersOutfit', function()
-    print('Player:GetCharactersOutfit')
+    local identifier = GetIdentifier(source, charid)
     local charappearance  = GetCharSkin(source, charid)
-        if charappearance then 
-        TriggerClientEvent('Player:LoadCharacterOutfit', source, charappearance)
+        if firstspawn then 
+            TriggerClientEvent('Player:CreateNewCharacterOutfit', source)   
+            firstspawn = false
         else
-        TriggerClientEvent('Player:CreateNewCharacterOutfit', source)    
+            TriggerClientEvent('Player:LoadCharacterOutfit', source, charappearance)
         end
 end)
 
