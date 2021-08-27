a558dd7373154c64dffb85044186981673b87291
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-27 04:04:10 +0100

    Multichar updates

--- linus/fxmanifest.lua
+++ linus/fxmanifest.lua
@@ -2,15 +2,26 @@ fx_version 'cerulean'
 game 'gta5'
 
 client_scripts { 
-    'client/cl_events.lua',
+    
     'client/cl_functions.lua',
+    'client/cl_events.lua',
     'client/cl_main.lua'
 
 } 
 
 server_scripts {
 
-    'server/functions.lua',
-    'server/events.lua',
-    'server/server.lua'
+    'server/sv_functions.lua',
+    'server/sv_events.lua',
+    'server/sv_server.lua'
+}
+
+ui_page "html/index.html"
+
+files {
+    'html/index.html',
+    'html/index.js',
+    'html/index.css',
+    'html/reset.css',
+    'html/assets/*.png'
 }
