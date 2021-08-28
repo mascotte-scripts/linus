2ca0635709273288889ab702a0d6df07d6cc8eb3
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-28 22:26:49 +0100

    Update sv_events.lua

--- linus/server/sv_events.lua
+++ linus/server/sv_events.lua
@@ -1,78 +1,41 @@
-function GetIdentifier(source, charid)
-    if charid then
-        for k,v in ipairs(GetPlayerIdentifiers(source)) do
-            if string.match(v, 'license:') then
-                local identifier = charid..':'..string.gsub(v, 'license:', '')
-                return identifier
-            end
-        end
-    else
-        for k,v in ipairs(GetPlayerIdentifiers(source)) do
-            if string.match(v, 'license:') then
-                local identifier = string.gsub(v, 'license:', '')
-                return identifier
-            end
-        end
+charid = 'char'
+firstspawn = false
+
+RegisterNetEvent('Multichar:InitiateServerSession')
+AddEventHandler('Multichar:InitiateServerSession', function()
+    local identifier = GetIdentifier(source)
+        if not identifier then
+            deferrals.done('Unknown Error. We could not find your identifier. Try restarting your FiveM game client')
+        else if identifier then
+            TriggerClientEvent('Multichar:InitiateClientSession', source)
+        end 
     end
-end
+end)
 
-function GetCharacterID(source, identifier)
-    return GetResourceKvpString(('users:%s:CharID'):format(identifier))
-end
-
-function GetCharSkin(source, charid)
-	local source = source
+RegisterNetEvent('Multichar:SetupCharacterData')
+AddEventHandler('Multichar:SetupCharacterData', function(CharacterData)
+     charid = CharacterData[6]
     local identifier = GetIdentifier(source, charid)
-    local appearance =  GetResourceKvpString(('users:%s:CharacterData:outfit'):format(identifier))
-    local charappearance = json.decode(appearance)
-	return charappearance
-end
+    local existingchars = GetIdentifier(source)
+    firstspawn = true
+    SaveCharacterDataToDB(source, identifier, charid, CharacterData)
+end)
 
-function GetCharacterData(source, identifier)
-    local CharacterData = {
-    ["firstname"] =	GetResourceKvpString(('users:%s:CharacterData:firstname'):format(identifier)),
-	["lastname"] =	GetResourceKvpString(('users:%s:CharacterData:lastname'):format(identifier)),
-	["gender"] = 	GetResourceKvpString(('users:%s:CharacterData:gender'):format(identifier)),
-	["nation"] = 	GetResourceKvpString(('users:%s:CharacterData:nation'):format(identifier)),
-	["dob"] = 	    GetResourceKvpString(('users:%s:CharacterData:dob'):format(identifier))
-    }
-    return CharacterData
-end
+RegisterNetEvent('Player:GetCharactersOutfit')
+AddEventHandler('Player:GetCharactersOutfit', function()
+    print('Player:GetCharactersOutfit')
+    local identifier = GetIdentifier(source, charid)
+    local charappearance  = GetCharSkin(source, charid)
+        if firstspawn then 
+            TriggerClientEvent('Player:CreateNewCharacterOutfit', source)   
+            firstspawn = false
+        else
+            TriggerClientEvent('Player:LoadCharacterOutfit', source, charappearance)
+        end
+end)
 
-function SaveCharacterDataToDB(source, identifier, charid, CharacterData)
-
-    print('Saving character data via KVS')
-        SetResourceKvp(('users:%s:'):format(identifier), identifier)
-        SetResourceKvp(('users:%s:CharID'):format(identifier), charid)
-		SetResourceKvp(('users:%s:CharacterData:firstname'):format(identifier), CharacterData[1])
-		SetResourceKvp(('users:%s:CharacterData:lastname'):format(identifier), CharacterData[2])
-		SetResourceKvp(('users:%s:CharacterData:gender'):format(identifier), CharacterData[3])
-		SetResourceKvp(('users:%s:CharacterData:nation'):format(identifier), CharacterData[4])
-		SetResourceKvp(('users:%s:CharacterData:dob'):format(identifier), CharacterData[5])
-    print('char saved')
-end
-
-function GetPlayerList()
-    local players = GetPlayers()
-    local playerList = {}
-    for i = 1, #players do
-      local playerId = tonumber(players[i])
-      local name = GetPlayerName(playerId)
-      playerList[playerId] = name
-    end
-    return playerList
-end
-
-function getPlayerFromIdentifier(identifier)
-	local players = GetPlayers()
-	for i = 1, #players do
-		local playerId = tonumber(players[i])
-		for _, id in pairs(GetPlayerIdentifiers(playerId)) do
-			if id == identifier then
-				return playerId
-			end
-		end
-	end
-	return -1
-end
-exports('getPlayerFromIdentifier', 'getPlayerByIdentifier')
+RegisterNetEvent('Player:SaveCharacterOutfit')
+AddEventHandler('Player:SaveCharacterOutfit', function(appearance)
+    local identifier = GetIdentifier(source, charid)
+        SetResourceKvp(('users:%s:CharacterData:outfit'):format(identifier), json.encode(appearance))     
+end)
