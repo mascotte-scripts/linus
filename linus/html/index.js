581107cb490912d140ee98d9baa596b13e0f2e8e
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-08-29 06:26:48 +0100

    Update index.js

--- linus/html/index.js
+++ linus/html/index.js
@@ -18,8 +18,8 @@ async function post(url, data) {
   
     if (item.type === 'ui') {
       const site = document.getElementById('site-container')
-      document.getElementById('char-slot-1-name').innerHTML="Lorem Ipsum";
-      
+      document.getElementById('char-slot-1-name').innerHTML="Bob Dole";
+      document.getElementById('char-slot-2-name').innerHTML="Rob Dole";
       if (item.status == true) {
         site.style.display = 'block';
       } else {
@@ -28,6 +28,8 @@ async function post(url, data) {
     }
   })
 
+  const charexists = false
+  activecharid = 'char'
 
   function SetCharIdToPlayer(charid) {
     if (charid == 1) {
@@ -41,18 +43,105 @@ async function post(url, data) {
       activecharid == "char3"
     } else if (charid == 4) {
       console.log('char id is 4')
-      activecharid == "char4"
+      activecharid == "char4";
     }
   }
 
-  function ToggleUiItemDisplay(module) {
-    var x = document.getElementById(module);
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+  function ToggleUiItemDisplay1(module) {
+
+    const charslot = document.getElementById('char-slot-1-name').innerHTML;
+    console.log(charslot)
+
+    if (charslot == "Slot Empty") { 
+      var x = document.getElementById(module);
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      } 
+  } else { 
+    var loadcharbox = document.getElementById('loadchar-form');
+      console.log('do something')
+     LoadCharacterDataOnNUI(1)
+      if (loadcharbox.style.display === "block") {
+        loadcharbox.style.display = "none";
+      } else {
+        loadcharbox.style.display = "block";
+      } 
     }
   }
+
+  function ToggleUiItemDisplay2(module) {
+
+    const charslot = document.getElementById('char-slot-2-name').innerHTML;
+    console.log(charslot)
+
+    if (charslot == "Slot Empty") { 
+      var x = document.getElementById(module);
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      } 
+  } else { 
+    LoadCharacterDataOnNUI(2)
+    var loadcharbox = document.getElementById('loadchar-form');
+      console.log('do something')
+      if (loadcharbox.style.display === "block") {
+    
+        loadcharbox.style.display = "none";
+      } else {
+        loadcharbox.style.display = "block";
+      } 
+    }
+  }
+
+  function ToggleUiItemDisplay3(module) {
+
+    const charslot = document.getElementById('char-slot-3-name').innerHTML;
+    console.log(charslot)
+
+    if (charslot == "Slot Empty") { 
+      var x = document.getElementById(module);
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      } 
+  } else { 
+    var loadcharbox = document.getElementById('loadchar-form');
+      console.log('do something')
+      if (loadcharbox.style.display === "block") {
+        loadcharbox.style.display = "none";
+      } else {
+        loadcharbox.style.display = "block";
+      } 
+    }
+  }
+
+  function ToggleUiItemDisplay4(module) {
+
+    const charslot = document.getElementById('char-slot-4-name').innerHTML;
+    console.log(charslot)
+
+    if (charslot == "Slot Empty") { 
+      var x = document.getElementById(module);
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      } 
+  } else { 
+    var loadcharbox = document.getElementById('loadchar-form');
+      console.log('do something')
+      if (loadcharbox.style.display === "block") {
+        loadcharbox.style.display = "none";
+      } else {
+        loadcharbox.style.display = "block";
+      } 
+    }
+  }
+
 
   function CreateCharacterData() {
     const CharacterData = [];
@@ -63,4 +152,63 @@ async function post(url, data) {
         CharacterData[4] = document.getElementById('creation-input-date').value;
         CharacterData[5] = activecharid;
             post('https://linus/SetCharacterData', CharacterData);
+  }
+
+  function LoadCharacterIntoGame(bool) {
+    if (bool == true) {
+      post('https://linus/LoadCharacterData', activecharid);
+    } else {
+      console.log('Something Went Wrong..')
+    }
+  }
+  
+  function LoadCharacterDataOnNUI(charid) {
+    if (charid == 1) { 
+
+    var charslotnumber = "I"
+    var charslotnumberHTML = '<h1 class="creation-form-title">Character '+charslotnumber+'</h1>';
+      document.getElementById('char-slot-1-data-char').innerHTML=charslotnumberHTML;
+
+    var loadedcharname ="Bob Dole";
+    var charnameHTML = '<h3 class="creation-form-title">'+loadedcharname+'</h3>';
+      document.getElementById('char-slot-1-data-name').innerHTML=charnameHTML;
+
+    var loadedcharjob ="Police Officer";
+    var charjobHTML = '<h3 class="creation-form-title">'+loadedcharjob+'</h3>';
+      document.getElementById('char-slot-1-data-job').innerHTML=charjobHTML;
+
+    var loadedcharbank ="$0.00";
+    var charbankHTML = '<h3 class="creation-form-title">'+loadedcharbank+'</h3>';
+      document.getElementById('char-slot-1-data-cash').innerHTML=charbankHTML;
+
+      var loadedcharactive ="Last Active: 5 days";
+      var charlastactiveHTML = '<h3 class="creation-form-title creation-form-title d-flex justify-content-center mt-2">'+loadedcharactive+'</h3>';
+        document.getElementById('char-slot-1-data-lastactive').innerHTML=charlastactiveHTML;
+        
+     
+  } else { 
+     if (charid == 2) {
+
+      var charslotnumber = "II"
+      var charslotnumberHTML = '<h1 class="creation-form-title">Character '+charslotnumber+'</h1>';
+        document.getElementById('char-slot-1-data-char').innerHTML=charslotnumberHTML;
+  
+      var loadedcharname ="Rob Dole";
+      var charnameHTML = '<h3 class="creation-form-title">'+loadedcharname+'</h3>';
+        document.getElementById('char-slot-1-data-name').innerHTML=charnameHTML;
+  
+      var loadedcharjob ="Medic";
+      var charjobHTML = '<h3 class="creation-form-title">'+loadedcharjob+'</h3>';
+        document.getElementById('char-slot-1-data-job').innerHTML=charjobHTML;
+  
+      var loadedcharbank ="$10.00";
+      var charbankHTML = '<h3 class="creation-form-title">'+loadedcharbank+'</h3>';
+        document.getElementById('char-slot-1-data-cash').innerHTML=charbankHTML;
+  
+        var loadedcharactive ="Last Active: 15 days";
+        var charlastactiveHTML = '<h3 class="creation-form-title creation-form-title d-flex justify-content-center mt-2">'+loadedcharactive+'</h3>';
+          document.getElementById('char-slot-1-data-lastactive').innerHTML=charlastactiveHTML;
+
+      }
+    }
   }
