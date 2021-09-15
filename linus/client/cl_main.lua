701f0b39a6f20d03c23e5b90585f489acec9916f
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-09-15 15:13:26 +0100

    Update cl_main.lua

--- linus/client/cl_main.lua
+++ linus/client/cl_main.lua
@@ -8,3 +8,18 @@ Citizen.CreateThread(function()
 		end
 	end
 end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsControlJustReleased(0,38) then
+			print('I like turtles')
+			local result = TriggerServerCallback('pmc-test:testingAwesomeCallback')
+			if result == 'Unemployed' then
+				print('No work here ese')
+			else
+				print("Didn't go as planned..")
+			end
+ 		end
+	end
+end)
