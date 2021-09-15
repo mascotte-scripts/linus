644ea03f97792e72e847bf496c4df0dc9ef315c5
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-09-15 22:57:43 +0100

    Update cl_functions.lua

--- linus/client/cl_functions.lua
+++ linus/client/cl_functions.lua
@@ -29,3 +29,23 @@ function CreateNewPlayerAppearance()
 		end
 	end, config)
 end
+
+function SetHUDAccountBalance(source, type, amount)
+	if type == 'wallet' then
+		local playerid = GetPlayerServerId(source)
+		local stringbalance = NetworkGetStringWalletBalance(playerid)
+		local intbalance = stringbalance:sub(2)
+		local sum = amount
+		local moneyType = wallet
+		StatSetInt(GetHashKey("MP0_WALLET_BALANCE"), math.floor(sum), true)
+	elseif type == 'bank' then
+		local playerid = GetPlayerFromServerId()
+		local stringbalance = NetworkGetStringBankBalance(playerid)
+		local intbalance = stringbalance:sub(2)
+		local sum = amount
+		local moneyType = bank
+		StatSetInt(GetHashKey("BANK_BALANCE"), math.floor(sum), true)
+	else
+		print('An unknown error occured while attempting to finding this account')
+	end
+end
