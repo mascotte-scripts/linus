ff8e248cc9616a83f5d5ca69ce9b86403ee9986b
KingMascotte <59978363+KingMascotte@users.noreply.github.com>
2021-09-15 23:05:26 +0100

    Update fxmanifest.lua

--- linus/fxmanifest.lua
+++ linus/fxmanifest.lua
@@ -3,6 +3,8 @@ game 'gta5'
 
 client_scripts { 
     
+    'client/cl_cache',
+    'client/misc.lua',
     'client/cl_functions.lua',
     'client/cl_events.lua',
     'client/cl_main.lua'
@@ -11,10 +13,13 @@ client_scripts {
 
 server_scripts {
 
+    'server/misc.lua',
     'server/sv_functions.lua',
     'server/sv_events.lua',
     'server/sv_server.lua'
 }
+
+shared_script 'shared/import.lua'
 
 ui_page "html/index.html"
 
