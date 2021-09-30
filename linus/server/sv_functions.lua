c7c23449d98aabbb1db16cccd0300845861703df
NotSomething0 <programming.unconsociated@aleeas.com>
2021-09-30 09:05:31 -0500

    feat(sv_functions) Additional helper functions

--- linus/server/sv_functions.lua
+++ linus/server/sv_functions.lua
@@ -1,3 +1,5 @@
+IDENTIFIER_CACHE = {}
+
 GetIdentifier = function(source, charid)
     if charid then
         for k,v in ipairs(GetPlayerIdentifiers(source)) do
@@ -24,14 +26,14 @@ GetCharSkin = function(identifier)
 end
 
 SaveCharSkinToDB = function(identifier, appearance)
-	SetResourceKvp(('%s:CharacterData:outfit'):format(identifier), json.encode(appearance))  
+	SetResourceKvp(('%s:CharacterData:outfit'):format(identifier), json.encode(appearance))
 end
 exports('SaveCharSkinToDB', SaveCharSkinToDB)
 
 SaveCharacterDataToDB = function(DbId, identifier, CharacterData)
     local data = json.encode(CharacterData)
     local job = GetConvar("DefaultJob", "Unemployed")
-	return SetResourceKvp(('%s:CharacterData:chardetails'):format(identifier), data) and  SetResourceKvp(('%s:CharacterData:job'):format(DbId), job) 
+	return SetResourceKvp(('%s:CharacterData:chardetails'):format(identifier), data) and  SetResourceKvp(('%s:CharacterData:job'):format(DbId), job)
 end
 
 GetPlayerList = function()
@@ -60,7 +62,7 @@ exports('GetBalance', GetBalance)
 AddAccountMoney = function(playerId, identifier, account, amount)
     local wallet = GetBalance(identifier, 'wallet')
     local bank = GetBalance(identifier, 'bank')
-    if account == 'wallet' then 
+    if account == 'wallet' then
             local sum = wallet + amount
             local newbalance = SetResourceKvpInt(('%s:CharacterData:wallet'):format(identifier), sum)
         TriggerClientEvent('Player:UpdateHudWalletBalance', playerId)
@@ -77,7 +79,7 @@ exports('AddAccountMoney', AddAccountMoney)
 RemoveAccountMoney = function(playerId, identifier, account, amount)
     local wallet = GetBalance(identifier, 'wallet')
     local bank = GetBalance(identifier, 'bank')
-    if account == 'wallet' then 
+    if account == 'wallet' then
             local sum = wallet - amount
             local newbalance = SetResourceKvpInt(('%s:CharacterData:wallet'):format(identifier), sum)
         TriggerClientEvent('Player:UpdateHudWalletBalance', playerId)
@@ -129,6 +131,49 @@ GetCharacterJob = function(playerId)
    return GetResourceKvpString(('%s:CharacterData:job'):format(playerId))
 end
 
+-- @param netId | number | players serverId
+-- @parma temporary | bool | if the netId is temporary ie assigned during playerConnecting
+-- @return table | A table containg the specified players identifiers exclduing ip
+function getAllPlayerIdentifiers(netId, temporary)
+  if not IDENTIFIER_CACHE[netId] then
+    local identifiers = {}
+
+    for i = 1, GetNumPlayerIdentifiers(netId) - 1 do
+      local raw = GetPlayerIdentifier(i)
+      local idx, value = raw:match("^([^:]+):(.+)$")
+
+      if idx ~= 'ip' then
+        identifiers[idx] = value
+      end
+    end
+
+    if temporary then
+      return identifiers
+    end
+
+    IDENTIFIER_CACHE[netId] = identifiers
+  end
+
+  return IDENTIFIER_CACHE[netId]
+end
+exports('getAllPlayerIdentifiers', 'getAllPlayerIdentifiers')
+
+-- @param identifier | string | raw identifier without suffix ie: suffix:identifier
+-- @return netId | number | player netId or -1 if no player is found
+function getPlayerFromIdentifier(identifier)
+	local players = GetPlayers()
+	for i = 1, #players do
+		local playerId = tonumber(players[i])
+		for _, id in pairs(IDENTIFIER_CACHE[playerId]) do
+			if id == identifier then
+				return playerId
+			end
+		end
+	end
+	return -1
+end
+exports('getPlayerFromIdentifier', 'getPlayerByIdentifier')
+
 RegisterCommand('giveaccountmoney', function(source, args)
     if args[1] and args[2] and args[3] then
         local account = args[1]
@@ -136,7 +181,7 @@ RegisterCommand('giveaccountmoney', function(source, args)
         local amount = tonumber(args[3])
         local getIdentifier = GetIdentifierFromDbId(pid)
         local playerId = GetServerIdFromIdentifier(getIdentifier)
-        AddAccountMoney(playerId, getIdentifier, account, amount) 
+        AddAccountMoney(playerId, getIdentifier, account, amount)
     else
         print('Invalid Usage - Format: /giveaccountmoney [bank/wallet] [DbId] [Amount]')
     end
@@ -149,7 +194,7 @@ RegisterCommand('removeaccountmoney', function(source, args)
         local amount = tonumber(args[3])
         local getIdentifier = GetIdentifierFromDbId(pid)
         local playerId = GetServerIdFromIdentifier(getIdentifier)
-        RemoveAccountMoney(playerId, getIdentifier, account, amount) 
+        RemoveAccountMoney(playerId, getIdentifier, account, amount)
     else
         print('Invalid Usage - Format: /removeaccountmoney [bank/wallet] [DbId] [Amount]')
     end
